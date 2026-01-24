000095a62d4230dd350fbbd74ab158a56fd29d89
unknown <pranjalkashid851@gmail.com>
2026-01-24 15:51:37 -0800

    1st commit to DevTinder

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express =require("express")
+const app=express()
+
+app.listen(3000 , ()=>
+{console.log("listening on port 3000")}
+);
+app.use("/test",(req,res) =>{
+    res.send("Hello test")
+
+})
+app.use("/",(req,res)=>{
+    res.send("Hello Home Page!!")
+})
+
+
